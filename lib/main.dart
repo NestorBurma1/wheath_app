0138fad0480a00f5a0d53ffc296a1cb543ba536f
Oleksii Khoroshun <alexeyhoroshunvk@gmail.com>
2019-12-08 18:10:16 +0200

    Directory to asset images need to be in main app folder, not in "lib" folder.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:wheath_app/provaider/weather_provaider.dart';
 
-
-void main() async {
-  Map data = await WeatherProvaider().getAllData();
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Weather application'),
-          backgroundColor: Colors.deepOrange,
+Future<void> main() async {
+  final Map data = await WeatherProvaider().getAllData();
+  print('Result is ' + data['city']['name'].toString());
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather application'),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Container(
+        height: 800.0,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: 800.0,
+              child: Image(
+                image: const AssetImage('asset/israelsunset.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
         ),
       ),
-    )
-  );
+    ),
+  ));
 }
